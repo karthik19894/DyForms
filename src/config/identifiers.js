5d4540da645f52008a7c2fa18a00bc876a5a22c5
Karthik Ruthiravelu <karthikruthiravelu@Karthiks-MacBook-Air.local>
2019-12-01 00:53:14 +0530

    restructured fields and identifiers.

---
+++ src/config/identifiers.js
@@ -0,0 +1,16 @@
+const identifiers={
+    createdBy:"createdBy",
+    description:"description",
+    severity:"severity",
+    status:"status",
+    cancelledReason:"cancelledReason",
+    cancelledOthersDescription:"cancelledOthersDescription",
+    comments:"comments",
+    cancelled:"cancelled",
+    completed:"completed",
+    others:"others",
+    endUser:"endUser"
+
+};
+
+export default identifiers;
